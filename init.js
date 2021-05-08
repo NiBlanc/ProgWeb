4ea99caee4233deece52e0c7bf10ca9e6aed5865
NicolasBlancEnseirb <nblanc003@enseirb-matmeca.fr>
2021-05-09 01:23:09 +0200

    Ajout table database user

--- init.js
+++ init.js
@@ -13,20 +13,40 @@ async function createCategories(db){
 }
 
 async function createPosts(db){
-  const insertRequest = await db.prepare("INSERT INTO posts(name, content, category) VALUES(?, ?, ?)")
+  const insertRequest = await db.prepare("INSERT INTO posts(name, content, category, author_id) VALUES(?, ?, ?, ?)")
   const contents = [{
     name: "Article 1",
     content: "Lorem lipsum, Lorem lipsum Lorem lipsum Lorem lipsum",
-    category: 1
+    category: 1,
+    author_id: 1
   },
     {
       name: "Article 2",
       content: "Lorem lipsum, Lorem lipsum Lorem lipsum Lorem lipsum",
-      category: 2
+      category: 2,
+      author_id: 1
     }
   ]
   return await Promise.all(contents.map(post => {
-    return insertRequest.run([post.name, post.content, post.category])
+    return insertRequest.run([post.name, post.content, post.category, post.author_id])
+  }))
+}
+
+async function createUsers(db){
+  const insertRequest = await db.prepare("INSERT INTO users(user_id, login, password) VALUES(?, ?, ?)")
+  const usernames = [{
+    user_id: 1,
+    login: "max",
+    password: "max"
+  },
+    {
+      user_id: 2,
+      login: "bob",
+      password: "bob"
+    }
+  ]
+  return await Promise.all(usernames.map(user => {
+    return insertRequest.run([user.user_id, user.login, user.password])
   }))
 }
 
@@ -37,16 +57,28 @@ async function createTables(db){
       cat_name varchar(255)
     )
   `)
+
+  const user = db.run(`
+  CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    login varchar(255),
+    password varchar(255)
+  )
+`)
+
   const post = db.run(`
         CREATE TABLE IF NOT EXISTS posts(
           id INTEGER PRIMARY KEY,
           name varchar(255),
           category int,
           content text,
+          author_id int,
+          FOREIGN KEY(author_id) REFERENCES users(user_id),
           FOREIGN KEY(category) REFERENCES categories(cat_id)
         )
   `)
-  return await Promise.all([cat,post])
+
+  return await Promise.all([cat,post,user])
 }
 
 
@@ -64,4 +96,5 @@ async function dropTables(db){
   await createTables(db)
   await createCategories(db)
   await createPosts(db)
+  await createUsers(db)
 })()
